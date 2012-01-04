e471f19b98f21e05b813c92765158f5fd4098e02
bigbigluo <bigbigluo@gmail.com>
2012-01-04 01:43:50 +0000

    seperated stress calculator for X dim

---
+++ src/calc.f90
@@ -0,0 +1,34 @@
+!Calculate stress etc.
+
+module calc
+
+  implicit none
+  private
+
+  public compute_stress
+
+contains
+
+subroutine compute_stress(pb)
+
+  use problem_class
+  type(problem_type) :: pb
+
+  ! compute shear stress rate from elastic interactions
+  ! compute_stress depends on dimension (0D, 1D, 2D fault)
+
+  if pb%mesh%kind == 0 then  ! 0D or 1D fault 
+    if (pb%mesh%nn > 1) then   ! 1D fault
+      pb%dtau_dt = pb%v_star-pb%v
+      pb%dtau_dt( pb%mesh%nn+1 : pb%kernel%k2f%nnfft ) = 0d0 
+      call rdft(pb%kernel%k2f%nnfft,1,pb%dtau_dt,pb%kernel%k2f%m_fft%iworkfft,pb%kernel%k2f%m_fft%rworkfft)
+      pb%dtau_dt = pb%kernel%k2f%kernel * pb%dtau_dt
+      call rdft(pb%kernel%k2f%nnfft,-1,pb%dtau_dt,pb%kernel%k2f%m_fft%iworkfft,pb%kernel%k2f%m_fft%rworkfft)
+    else
+      pb%dtau_dt(1) = pb%kernel%k2f%kernel(1)*( pb%v_star(1)-pb%v(1) )   !0D fault
+    endif
+  endif
+  
+end subroutine compute_stress
+
+end module calc
