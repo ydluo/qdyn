a367185034da96c31fd02b2b51327263523eb609
ampuero@caltech.edu <ampuero@caltech.edu@087eadcf-8de5-7d04-1dcd-5ebf254e7124>
2012-01-11 08:54:05 +0000

    fixed dtau_dt; arranged compute_stress

--- trunk/src/calc.f90
+++ trunk/src/calc.f90
@@ -12,23 +12,30 @@ contains
 ! compute shear stress rate from elastic interactions
 ! compute_stress depends on dimension (0D, 1D, 2D fault)
 !
-subroutine compute_stress(pb,yt)
+subroutine compute_stress(dtau_dt,K,v,v_pl)
 
   use fftsg, only : my_rdft
-  use problem_class
+  use problem_class, only :: kernel_type
 
-  type(problem_type), intent(inout)  :: pb
-  double precision , intent(inout) :: yt(pb%neqs*pb%mesh%nn)
+  type(kernel_type), intent(inout)  :: K
+  double precision , intent(out) :: dtau_dt(:)
+  double precision , intent(in) :: v(:)
 
-  if (pb%mesh%kind == 0) then  ! 0D or 1D fault 
-    if (pb%mesh%nn > 1) then   ! 1D fault
-      pb%dtau_dt( 1 :  pb%mesh%nn) = pb%v_star - yt(2::pb%neqs) 
-      pb%dtau_dt( pb%mesh%nn+1 : pb%kernel%k2f%nnfft ) = 0d0  
-      call my_rdft(1,pb%dtau_dt,pb%kernel%k2f%m_fft) 
-      pb%dtau_dt = pb%kernel%k2f%kernel * pb%dtau_dt
-      call my_rdft(-1,pb%dtau_dt,pb%kernel%k2f%m_fft)
+  double precision :: tmp(K%k2f%nnfft)
+  integer :: nn
+
+  nn = size(v)
+
+  if (K%kind == 0) then  ! 0D or 1D fault 
+    if (nn > 1) then   ! 1D fault
+      tmp( 1 : nn ) = v_pl - v
+      tmp( nn+1 : K%k2f%nnfft ) = 0d0  
+      call my_rdft(1,tmp,K%k2f%m_fft) 
+      tmp = K%k2f%kernel * tmp
+      call my_rdft(-1,tmp,K%k2f%m_fft)
+      dtau_dt = tmp(1:nn)
     else
-      pb%dtau_dt(1) = pb%kernel%k2f%kernel(1)*( pb%v_star(1)-yt(2) )   !0D fault
+      dtau_dt(1) = K%k2f%kernel(1)*( v_pl(1)-v(2) )   !0D fault
     endif
   endif
   
