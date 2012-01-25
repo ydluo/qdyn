8768bf8c83084977c7ae8a34591cf19af5e96318
bigbigluo <bigbigluo@gmail.com>
2012-01-25 22:11:33 +0000

    added matlab scripts for plotting slip

--- src/plot3d.m
+++ src/plot3d.m
@@ -1,6 +1,6 @@
 
 clc;
-name='JP2D_HD';
+name='test_3d';
 mname=[name '.avi'];
 year=3600*24*365;
 for i=1:numel(ox1.t)
@@ -8,11 +8,13 @@ for i=1:numel(ox1.t)
   disp(['Plotting ', num2str(i),'/',num2str(numel(ox1.t))]);
   h1=figure(1);
   scatter3(p.X/1000,p.Y/1000,p.Z/1000,[],log10(ox1.v(:,i)/p.V_SS),'s','filled');
-  caxis([-2 10]);
-  colorbar;
+  caxis([-2 8]);
+  colorbar('NorthOutside');
   axis equal;
-  title(['time = ', num2str(ox1.t(i)/year),' year']);
+  title(['time = ', num2str(ox1.t(i)/year,'%15.8f'),' year']);
   zlabel('Depth : km');
+  xlabel('Location along-strike: km');
+  ylabel('Location Y: km');
 %  print(h1,'-djpeg','-r1000',[name, num2str(i), '.jpg']);
   print(h1,'-dpdf',[name, num2str(i), '.pdf']);
 %  mov(i)=getframe;
