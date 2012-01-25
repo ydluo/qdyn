7921c9df23870e5da8cabc4e9a50ee94efd3c545
bigbigluo <bigbigluo@gmail.com>
2012-01-25 11:35:00 +0000

    modified plot3d

--- src/plot3d.m
+++ src/plot3d.m
@@ -1,6 +1,6 @@
 
 clc;
-name='JP2D';
+name='JP2D_HD';
 mname=[name '.avi'];
 year=3600*24*365;
 for i=1:numel(ox1.t)
@@ -8,12 +8,13 @@ for i=1:numel(ox1.t)
   disp(['Plotting ', num2str(i),'/',num2str(numel(ox1.t))]);
   h1=figure(1);
   scatter3(p.X/1000,p.Y/1000,p.Z/1000,[],log10(ox1.v(:,i)/p.V_SS),'s','filled');
-  caxis([-2 8]);
+  caxis([-2 10]);
   colorbar;
   axis equal;
   title(['time = ', num2str(ox1.t(i)/year),' year']);
   zlabel('Depth : km');
-  print(h1,'-djpeg','-r600',[name, num2str(i), '.jpg']);
+%  print(h1,'-djpeg','-r1000',[name, num2str(i), '.jpg']);
+  print(h1,'-dpdf',[name, num2str(i), '.pdf']);
 %  mov(i)=getframe;
   clf(h1);
 end
