0d0c8a515c17e025cf08a2a177d28e3a2994af50
ampuero <ampuero@caltech.edu>
2012-02-01 01:33:20 +0000

    increased precision of time output in ox

--- src/output.f90
+++ src/output.f90
@@ -144,8 +144,9 @@ subroutine ox_write(pb)
   integer :: ixout
 
   write(pb%ox%unit,'(2a,2i8,e14.6)')'# x v theta',' V./V dtau tau_dot slip ',pb%it,pb%ot%ivmax,pb%time
+! JPA: this output should also contain y and z
   do ixout=1,pb%mesh%nn,pb%ox%nxout
-    write(pb%ox%unit,'(8e15.7)') pb%mesh%x(ixout),pb%time,pb%v(ixout),   &
+    write(pb%ox%unit,'(e15.7,e24.16,6e15.7)') pb%mesh%x(ixout),pb%time,pb%v(ixout),   &
       pb%theta(ixout),pb%dv_dt(ixout)/pb%v(ixout),pb%tau(ixout),   &
       pb%dtau_dt(ixout),pb%slip(ixout)
   enddo
