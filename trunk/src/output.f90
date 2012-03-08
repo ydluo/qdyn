6cd867b0de6c4c7b736bf9a71740b03642593448
bigbigluo@gmail.com <bigbigluo@gmail.com@087eadcf-8de5-7d04-1dcd-5ebf254e7124>
2012-03-08 23:38:38 +0000

    improved compatibility to old ox output format

--- trunk/src/output.f90
+++ trunk/src/output.f90
@@ -148,12 +148,11 @@ subroutine ox_write(pb)
   integer :: ixout
 
   if (pb%ox%i_ox_seq == 0) then
-    write(pb%ox%unit,'(3i10,e24.14)') pb%it,pb%ot%ivmax,pb%ox%count,pb%time
-    write(pb%ox%unit,'(2a)') '#  x  y  z  t  v  theta','  V./V  dtau  tau_dot  slip '
+    write(pb%ox%unit,'(2a,2i8,e14.6)')'# x v theta',' V./V dtau tau_dot slip ',pb%it,pb%ot%ivmax,pb%time
+  ! JPA: this output should also contain y and z
     do ixout=1,pb%mesh%nn,pb%ox%nxout
-      write(pb%ox%unit,'(3e15.7,e24.14,6e15.7)')       &
-        pb%mesh%x(ixout),pb%mesh%y(ixout),pb%mesh%z(ixout),pb%time,    &
-        pb%v(ixout),pb%theta(ixout),pb%dv_dt(ixout)/pb%v(ixout),pb%tau(ixout),   &
+      write(pb%ox%unit,'(e15.7,e24.16,6e15.7)') pb%mesh%x(ixout),pb%time,pb%v(ixout),   &
+        pb%theta(ixout),pb%dv_dt(ixout)/pb%v(ixout),pb%tau(ixout),   &
         pb%dtau_dt(ixout),pb%slip(ixout)
     enddo
   else
