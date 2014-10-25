51f033df8c602abf5d3a0055f546249366740f57
bigbigluo <bigbigluo@gmail.com>
2014-10-25 10:46:30 +0000

    matlab wrapper refreshed, added examples for Nov,2014 workshop

---
+++ examples/double_asperities/double_asperities.m
@@ -0,0 +1,92 @@
+% a brittle asperity (velocity-weakening) surrounded by creep (velocity-strengthening)
+
+Lasp = 4; % asperity "size" normalized by Lc = mu*Dc/((b-a)*sigma)
+Lasp_2 = 2; % asperity "size" normalized by Lc = mu*Dc/((b-a)*sigma)
+L = 5;    % total fault size normalized by Lasp
+AB_RATIO = 0.6; % a/b at the center of the asperity. Outside: a/b = 2-AB_RATIO
+RUN_OR_READ = 1; % 1 = run the simulation, 0 = read a pre-computed simulation 
+RESOLUTION = 7;	% minimum number of nodes per Lb-length, dx/Lb>RESOLUTION, usually 9. 
+  		% For large a/b and L/Lc, RESOLUTION=7 might be ok
+THETA_LAW=1;
+
+%-----------
+
+filename = 'example_1.mat';
+
+year = 3600*24*365;
+%if exist('qdyn')~=2, addpath ~/2D_RUPTURE/RATE_AND_STATE/qdyn/ ; end
+p = qdyn('set');
+p.THETA_LAW=THETA_LAW;
+% characteristic half-lengths
+Lb = p.MU.*p.DC./p.SIGMA./p.B;
+Lnuc = 1.3774*Lb;
+Lc = Lb/(1-AB_RATIO);
+Linf = 1/pi *(1-AB_RATIO)^2 *Lb;
+
+Lasp = Lasp*Lc;
+Lasp_2 = Lasp_2*Lc;
+
+L = L*Lasp;
+
+if RUN_OR_READ
+
+  p.L = L;
+  %p.W = 1000*p.L;
+  p.FINITE=1;
+  p.N = 2^nextpow2(RESOLUTION*p.L/Lb); 
+  dx=p.L/p.N;
+  Lb_over_dx = Lb/dx
+  p.ACC = 1e-10;
+  
+  p = qdyn('set',p);
+  
+  b=p.B;
+  a=p.A;
+  
+  p.B(1:1:p.N)=a/(2-AB_RATIO);
+  p.A(1:1:p.N)=a;
+  
+  p.A = p.A - b*AB_RATIO*exp(-((p.X-(Lasp+Lasp_2)*0.6)/Lasp*2).^6); 
+  p.A = p.A - b*AB_RATIO*exp(-((p.X+(Lasp+Lasp_2)*0.6)/Lasp_2*2).^6); 
+  
+  p.TMAX = 8 * year; % 60?
+  p.NTOUT=200;
+  p.NXOUT=1;
+  p.NSTOP=0;
+  %p.V_0 = 1.01*p.V_SS ;
+  p.V_0 = ( 1.+0.01*exp( -(p.X/Lasp*2).^6 ) )*p.V_SS ;
+  p.V_0 = p.V_0/mean(p.V_0)*p.V_SS;
+  [p,ot1,ox1] = qdyn('run',p) ;
+  
+  
+%  p.TMAX = 0.4*year; 
+%  p.NTOUT=10;
+  p.TMAX = 2 *year;  
+  p.NTOUT= 10;
+
+  p.V_0 = ox1.v(:,end);
+  p.TH_0= ox1.th(:,end);
+  %p.V_0 =  (ox1.v(:,end)+ox1.v(end:-1:1,end))/2;
+  %p.TH_0=  (ox1.th(:,end)+ox1.th(end:-1:1,end))/2;
+  [p,ot,ox]=qdyn('run',p);
+  
+  save(filename)
+
+else
+  load(filename)
+end
+
+%--- warmup cycles
+if exist('ot1','var')
+figure(1)
+semilogy(ot1.t/year,ot1.v)
+xlabel('Time (years)')
+legend('V_{max}','Location','Best')
+title('Warming cycles')
+end
+
+
+
+
+
+
