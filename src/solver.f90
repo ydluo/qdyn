9baf56e8cdc9c180e711c327eb9d6e553d14af9b
Martijn van den Ende <m.p.a.vandenende@uu.nl>
2017-09-11 10:54:12 +0200

    SEISMIC: fixed bug related to non-scalar mesh size dx

--- src/solver.f90
+++ src/solver.f90
@@ -247,16 +247,16 @@ subroutine update_field(pb)
 
   ! update potency and potency rate
   if (pb%mesh%dim == 0 .or. pb%mesh%dim == 1) then
-    pb%ot%pot = sum(pb%slip) * pb%mesh%dx
-    pb%ot%pot_rate = sum(pb%v) * pb%mesh%dx
+    pb%ot%pot = sum(pb%slip * pb%mesh%dx)
+    pb%ot%pot_rate = sum(pb%v * pb%mesh%dx)
   else
     pb%ot%pot=0d0;
     pb%ot%pot_rate=0d0;
     do iw=1,pb%mesh%nw
       do ix=1,pb%mesh%nx
         i=(iw-1)*pb%mesh%nx+ix
-        pb%ot%pot = pb%ot%pot + pb%slip(i) * pb%mesh%dx * pb%mesh%dw(iw)
-        pb%ot%pot_rate = pb%ot%pot_rate + pb%v(i) * pb%mesh%dx * pb%mesh%dw(iw)
+        pb%ot%pot = pb%ot%pot + pb%slip(i) * pb%mesh%dx(i) * pb%mesh%dw(iw)
+        pb%ot%pot_rate = pb%ot%pot_rate + pb%v(i) * pb%mesh%dx(i) * pb%mesh%dw(iw)
       end do
     end do
   endif
