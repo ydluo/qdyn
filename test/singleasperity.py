4ab93fa24c722355320ce6d0e81e9d9eea97710c
Martijn van den Ende <martijnende@gmail.com>
2018-08-03 11:16:15 +0200

    Added single asperity test (RSF only)

--- test/singleasperity.py
+++ test/singleasperity.py
@@ -24,53 +24,101 @@ class TestSingleAsperity(AuxiliaryFunctions):
         self.pickle_file = os.path.join(self.dirname, pickle_name)
         pass
 
+    @staticmethod
+    def render_RSF_mesh(set_dict):
+
+        b = set_dict["SET_DICT_RSF"]["B"]
+        Dc = set_dict["SET_DICT_RSF"]["DC"]
+        mu = set_dict["MU"]
+        sigma = set_dict["SIGMA"]
+        V_pl = set_dict["V_PL"]
+
+        Lasp = 7
+        L = 5
+        ab_ratio = 0.9
+        cab_ratio = 1 - ab_ratio
+        resolution = 7
+
+        Lb = mu*Dc/(b*sigma)
+        Lc = Lb / cab_ratio
+        Lasp *= Lc
+        L *= Lasp
+
+        N = np.power(2, np.ceil(np.log2(resolution*L/Lb)))
+        x = np.linspace(-L/2, L/2, N, dtype=float)
+
+        a = b*(1 + cab_ratio*(1 - 2*np.exp(-(2*x/Lasp)**6)))
+
+        V0 = V_pl*(1 + 0.01*np.exp(-(2*x/Lasp)**6))
+        V0 = V0 * V_pl / np.mean(V0)
+
+        result = {
+            "N": int(N),
+            "L": L,
+            "a": a,
+            "V0": V0,
+        }
+
+        return result
+
     def run_test(self, mode):
 
         p = self.p
         set_dict = p.set_dict
-
-        N = np.power(2, 10)
         t_yr = 3600 * 24 * 365.0
 
-        set_dict["N"] = N
         set_dict["MESHDIM"] = 1
         set_dict["FINITE"] = 0
-        set_dict["TMAX"] = 100*t_yr
-        set_dict["L"] = 500.0
+        set_dict["TMAX"] = 10*t_yr
+        set_dict["NTOUT"] = 100
+        set_dict["NXOUT"] = 1
         set_dict["V_PL"] = 1e-9
+        set_dict["MU"] = 3e10
+        set_dict["W"] = 50e3
+        set_dict["SIGMA"] = 1e8
+        set_dict["ACC"] = 1e-10
+        set_dict["DTTRY"] = 100
+        set_dict["V_TH"] = 1e-2
+        set_dict["SOLVER"] = 1
 
-        if mode == "CNS":
-            set_dict["FRICTION_MODEL"] = "CNS"
-            dict_name = "SET_DICT_CNS"
-            set_dict[dict_name]["PHI_INI"] = 0.1
-            set_dict[dict_name]["TAU"] = 0.2*set_dict[dict_name]["SIGMA"]
+        # Setting some RSF parameters
+        set_dict["SET_DICT_RSF"]["A"] = 0.9e-2
+        set_dict["SET_DICT_RSF"]["B"] = 1e-2
+        set_dict["SET_DICT_RSF"]["DC"] = 4e-4
+        set_dict["SET_DICT_RSF"]["V_SS"] = set_dict["V_PL"]
+        set_dict["SET_DICT_RSF"]["TH_0"] = \
+            set_dict["SET_DICT_RSF"]["DC"] / set_dict["V_PL"]
 
-            # Feed our settings to QDYN and render mesh
-            p.settings(set_dict)
-            p.render_mesh()
+        result = self.render_RSF_mesh(set_dict)
+        set_dict["N"] = result["N"]
+        set_dict["L"] = result["L"]
 
-        elif mode == "RSF":
+        # Setting some CNS parameters
+        set_dict["SET_DICT_CNS"]["PHI_INI"] = 0.1
+        set_dict["SET_DICT_CNS"]["TAU"] = 0.2*set_dict["SIGMA"]
+
+        if mode == "RSF":
             set_dict["FRICTION_MODEL"] = "RSF"
-            dict_name = "SET_DICT_RSF"
-            set_dict[dict_name]["A"] = 0.01
-            set_dict[dict_name]["B"] = 0.009
-            set_dict[dict_name]["DC"] = 1e-4
-            set_dict[dict_name]["TH_0"] = 0.5*set_dict[dict_name]["DC"]/set_dict["V_PL"]
 
-            # Feed our settings to QDYN and render mesh
             p.settings(set_dict)
             p.render_mesh()
 
-            p.mesh_dict["B"][2*N//7:5*N//7] = 0.015
+            p.mesh_dict["A"] = result["a"]
+            p.mesh_dict["V_0"] = result["V0"]
 
         # Write input file
         p.write_input()
 
         # Run simulation
-        p.run(test=False)
+        p.run(test=True)
 
         # Get our results
         p.read_output()
+
+        # Store results
+        result_t = (p.ot["t"] - p.ot["t"].iloc[0]).values
+        result_var1 = p.ot["theta"].values
+        result_var2 = np.log10(p.ot["v_max"].values)
 
         self.test_results[mode] = {
             "t": result_t,
