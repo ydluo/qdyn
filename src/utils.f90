e83c996afb36571a3daf2a2ca52b8e31c7e9ceb0
Martijn van den Ende <martijnende@users.noreply.github.com>
2018-12-04 18:52:40 +0100

    Re-structured the packing/unpacking of variables (#16)
    Prior to the solver step, variables (theta, tau/v, etc.) are merged (packed) into a single variable and passed onto the solver subroutine. After the solver step, the resulting variable is split-up (unpacked) back into the original variables. This process is repeated on various locations in the code with some code duplication. The packing/unpacking procedure is now centralised as a subroutine in utils.f90

--- src/utils.f90
+++ src/utils.f90
@@ -1,10 +1,97 @@
-module utils 
+! Collection of auxiliary functions
 
+module utils
+
+  use problem_class, only : problem_type
   implicit none
+  public
  !   double precision, allocatable, save :: Vec
  !   public :: Vec
 
 contains
+
+!===============================================================================
+! Helper routine to pack variables for solver
+! Storage conventions:
+!
+! theta = yt(1::pb%neqs)
+! v or tau = yt(2::pb%neqs) (depending on friction law)
+! + feature specific variables
+!
+! dtheta/dt = dydt(1::pb%neqs)
+! dv/dt or dtau/dt = dydt(2::pb%neqs)
+! + feature specific variables
+!===============================================================================
+subroutine pack(yt, theta, main_var, sigma, theta2, P, pb)
+
+  type(problem_type), intent(inout) :: pb
+  double precision, dimension(pb%neqs*pb%mesh%nn), intent(out) :: yt
+  double precision, dimension(pb%mesh%nn), intent(in) :: theta, main_var, sigma
+  double precision, dimension(pb%mesh%nn), intent(in) :: theta2, P
+  integer :: nmax, ind_stress_coupling, ind_localisation, ind_tp
+
+  nmax = pb%neqs*pb%mesh%nn
+  ! Define the indices of yt and dydt based on which
+  ! features are requested (defined in input file)
+  ind_stress_coupling = 2 + pb%features%stress_coupling
+  ind_localisation = ind_stress_coupling + pb%features%localisation
+  ind_tp = ind_localisation + pb%features%tp
+
+  yt(1:nmax:pb%neqs) = theta
+  yt(2:nmax:pb%neqs) = main_var
+
+  if (pb%features%stress_coupling == 1) then
+    yt(ind_stress_coupling:nmax:pb%neqs) = sigma
+  endif
+
+  if (pb%features%localisation == 1) then
+    yt(ind_localisation:nmax:pb%neqs) = theta2
+  endif
+
+  if (pb%features%tp == 1) then
+    yt(ind_tp:nmax:pb%neqs) = P
+  endif
+
+end subroutine pack
+
+!===============================================================================
+! Helper routine to unpack variables from solver
+!===============================================================================
+subroutine unpack(yt, theta, main_var, sigma, theta2, P, pb)
+
+  type(problem_type), intent(inout) :: pb
+  double precision, dimension(pb%neqs*pb%mesh%nn), intent(in) :: yt
+  double precision, dimension(pb%mesh%nn) :: theta, main_var, sigma, theta2, P
+  integer :: nmax, ind_stress_coupling, ind_localisation, ind_tp
+
+  nmax = pb%neqs*pb%mesh%nn
+  ind_stress_coupling = 2 + pb%features%stress_coupling
+  ind_localisation = ind_stress_coupling + pb%features%localisation
+  ind_tp = ind_localisation + pb%features%tp
+
+  theta = yt(1:nmax:pb%neqs)
+  main_var = yt(2:nmax:pb%neqs)
+
+  if (pb%features%stress_coupling == 1) then
+    sigma = yt(ind_stress_coupling:nmax:pb%neqs)
+  else
+    sigma = pb%sigma
+  endif
+
+  if (pb%features%localisation == 1) then
+    theta2 = yt(ind_localisation:nmax:pb%neqs)
+  else
+    theta2 = 0d0
+  endif
+
+  if (pb%features%tp == 1) then
+    P = yt(ind_tp:nmax:pb%neqs)
+  else
+    P = 0d0
+  endif
+
+end subroutine unpack
+
 !
 !=====================================================================
 !
@@ -18,11 +105,11 @@ subroutine save_array(x,y,z,V,iproc,typ,nw,nx)
   integer :: i,j,iproc,nx,nw
 
   write(fileproc,'(a,i6.6,a)') 'snap_v_',iproc,typ
- 
+
   open(101,file=fileproc(1:len_trim(fileproc)),status='replace',form='formatted',action='write')
-  
+
   do i=1,nw*nx
-      write(101,'(4(D15.7))') x(i),y(i),z(i),V(i)  
+      write(101,'(4(D15.7))') x(i),y(i),z(i),V(i)
   enddo
 
   close(101)
@@ -42,12 +129,12 @@ subroutine save_vectorV(x,y,z,V,iproc,typ,nw,nx)
   integer :: i,j,iproc,nx,nw
 
   write(fileproc,'(a,i6.6,a)') 'snap_v_',iproc,typ
- 
+
   open(101,file=fileproc(1:len_trim(fileproc)),status='replace',form='formatted',action='write')
-  
+
   do i=1,nw
     do j=1,nx
-      write(101,'(4(D15.7))') x(i,j),y(i,j),z(i,j),V(i,j)  
+      write(101,'(4(D15.7))') x(i,j),y(i,j),z(i,j),V(i,j)
     enddo
   enddo
 
@@ -65,12 +152,12 @@ subroutine save_vector(V,iproc,typ,nw,nx)
   integer :: i,j,iproc,nx,nw
 
   write(fileproc,'(a,i6.6,a)') 'snap_v_',iproc,typ
- 
+
   open(101,file=fileproc(1:len_trim(fileproc)),status='replace',form='formatted',action='write')
-  
+
   do i=1,nw
     do j=1,nx
-      write(101,'(D15.7)') V(i,j)  
+      write(101,'(D15.7)') V(i,j)
     enddo
   enddo
 
@@ -88,13 +175,13 @@ subroutine save_vector3(V,iproc,typ,nwloc,nwglob,nx)
   integer :: i,j,k,iproc,nx,nwloc,nwglob
 
   write(fileproc,'(a,i6.6,a)') 'snap_v_',iproc,typ
- 
+
   open(101,file=fileproc(1:len_trim(fileproc)),status='replace',form='formatted',action='write')
-  
+
  do k=1,nwloc
   do i=1,nwglob
     do j=1,nx
-      write(101,'(D15.7)') V(k,i,j)  
+      write(101,'(D15.7)') V(k,i,j)
     enddo
   enddo
  enddo
